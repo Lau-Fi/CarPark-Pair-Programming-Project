05851e58e8207232fd6c44b9d721ba806f2af9b9
Laurence Beven <starryflight@outlook.com>
2024-03-22 13:53:18 +0000

    Our first pair project - functionality of A1 - done.

---
+++ a2_spot.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+CarPark Project
+Coursework for: 4009B Programming for applications
+
+
+@author: 123456789, 100363107
+@date:   19/03/2024
+
+"""
+
+class Spot:
+    def __init__(self, status, durations):
+        self.status = "Avaliable" 
+        self.durations = []
+        
+    def __str__(self):
+
+
+def parking(self, car, duration):
+    if self.status == "Available":
+        self.status = ""
---
+++ a8_simulator.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+CarPark Project
+Coursework for: 4009B Programming for applications
+
+
+@author: 123456789, 100363107
+@date:   19/03/2024
+
+"""
