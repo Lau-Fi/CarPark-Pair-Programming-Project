5fc842ddffbf70ad8c2df2fc8e92f2fba4bc7413
Laurence <laurenc33e@outlook.com>
2024-04-17 17:56:27 +0100

    doing more of 8 - but now matplotlib cannot be found despite a pip install!??!?!?!?!

--- a8_simulator.py
+++ a8_simulator.py
@@ -13,15 +13,16 @@ import a5_station as a5
 import a2_spot as a2 
 import a6_elot as a6 
 import a7_park as a7 
-
+import random
+import matplotlib.pyplot as plt
 
 stationInstance1 = a5.Station(2, 4.8, (16, 32))
 stationInstance2 = a5.Station(5, 4.8, (16, 32))
 stationInstance3 = a5.Station(8, 4.8, (16, 32))
 
-spotInstance1 = a2.Spot("Avaliable", [])
-spotInstance2 = a2.Spot("Unavaliable", [])
-spotInstance3 = a2.Spot("Avaliable", [])
+spotInstance1 = a2.Spot("Avaliable", [random.randint(10, 120)])
+spotInstance2 = a2.Spot("Unavaliable", [random.randint(10, 120)])
+spotInstance3 = a2.Spot("Avaliable", [random.randint(10, 120)])
 spotInstance4 = a2.Spot("Avaliable", [])
 spotInstance5 = a2.Spot("Unavaliable", [])
 spotInstance6 = a2.Spot("Avaliable", [])
@@ -29,3 +30,12 @@ spotInstance6 = a2.Spot("Avaliable", [])
 ELotInstance1 = a6.Elot(stationInstance1)
 ELotInstance2 = a6.Elot(stationInstance2)
 ELotInstance3 = a6.Elot(stationInstance3)
+
+cmPark = [spotInstance1, spotInstance2, spotInstance3, spotInstance4, spotInstance5, spotInstance6, ELotInstance1, ELotInstance2, ELotInstance3]
+
+a7.Park.toFile(cmPark)
+
+
+
+durationsFigure = plt.figure()
+plt.plot()
