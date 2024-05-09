074880820414d492284958c887c0d3ae5d578f89
Tucker55 <164914110+Tucker55@users.noreply.github.com>
2024-05-09 13:34:13 +0100

    a4

--- a4_card.py
+++ a4_card.py
@@ -14,37 +14,21 @@ class Card:
     numCards = 0
 
     def __init__(self, _credit):
-        """_summary_
-
-        Args:
-            _credit (_type_): _description_
-        """
+        # Initialize the Card object with a given credit value
         self._credit = _credit
 
     def __str__(self):
-        """_summary_
-
-        Returns:
-            _type_: _description_
-        """
+        # Return a string representation of the Card object
         return f"Card with {self.credit} credits available"
 
     @property
     def credit(self):
-        """_summary_
-
-        Returns:
-            _type_: _description_
-        """
+        # Get the credit value of the Card object
         return self._credit
 
     @credit.setter
     def credit(self, credit):
-        """_summary_
-
-        Args:
-            credit (_type_): _description_
-        """
+        # Set the credit value of the Card object, making sure it's not negative
         if credit >= 0:
             self._credit = credit
         else:
@@ -52,27 +36,15 @@ class Card:
             self._credit = 0
     
     def topup(self, value):
-        """_summary_
-
-        Args:
-            value (_type_): _description_
-
-        Returns:
-            _type_: _description_
-        """
+        # Add the given value to the credit of the Card object
         self.credit += value
         return self.credit
 
 
     def pay(self, amount_to_pay):
-        """_summary_
-
-        Args:
-            amount_to_pay (_type_): _description_
-
-        Returns:
-            _type_: _description_
-        """
+        # Check if the Card object has enough credit to pay the given amount
+        # If yes, deduct the amount from the credit and return True
+        # If no, return False
         if amount_to_pay <= self.credit:
             self.credit -= amount_to_pay
             return True
@@ -80,12 +52,18 @@ class Card:
             return False
 
 def tester():
+    # Create a Card object with an initial credit of 18
     card1 = Card(18)
+    # Top up the card with 10 credits
     card1.topup(10)
+    # Pay 3 credits from the card
     payed = card1.pay(3)
+    # Print the string representation of the card
     print(card1)
+    # Print whether the payment was successful or not
     print(payed)
 
 if __name__ == "__main__":
+    # Call the tester function when the module is run directly
     tester()
 
